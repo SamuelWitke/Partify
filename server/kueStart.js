96056c441bbed96196d8fd6f484f04b320ebd567
SamuelWitke <SamuelWitke@gmail.com>
2018-04-01 15:18:03 -0400

    heroku push

--- server/kueStart.js
+++ server/kueStart.js
@@ -1,25 +1,11 @@
 const kue = require('kue');
 const url = require('url')
 const redis = require('kue/node_modules/redis');
-const config = require("../.redis-labs.js")
-const project = require('../project.config')
-console.log(config)
-let redisHost;
-let redisPort;
-let redisKey;
-if (project.env === 'development') {
-    redisPort='6379';
-    redisHost='127.0.0.1';
-}else{
-    redisPort=config.Redis.redisPort;
-    redisHost=config.Redis.redisHost;
-    redisKey=config.Redis.redisKey;
-}
 
 kue.redis.createClient = () =>{
     return redis.createClient(
-    redisPort,
-    redisHost,
+    process.env.redisPort || "6379",
+    process.env.redisHost || "127.0.0.1",
     {
     'auth_pass': redisKey,
     'return_buffers': true
