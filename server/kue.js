84155bfe9dfbe01737f7513ff2b6cb99906de017
SamuelWitke <SamuelWitke@gmail.com>
2018-04-21 21:03:23 -0400

    refactor

--- server/kue.js
+++ server/kue.js
@@ -2,7 +2,19 @@ const logger = require('../build/lib/logger')
 const url = require('url')
 const admin = require('firebase-admin');
 const kue = require('kue')
-const jobs = kue.createQueue();
+const kueOptions = {};
+
+if(process.env.REDISCLOUD_URL) {
+    var redisUrl = url.parse(process.env.REDISCLOUD_URL);
+    kueOptions.redis = {
+       port: parseInt(redisUrl.port),
+       host: redisUrl.hostname
+    };
+    if(redisUrl.auth) {
+       kueOptions.redis.auth = redisUrl.auth.split(':')[1];
+    }
+}
+const jobs = kue.createQueue(kueOptions);
 
 const request = require('request');
 const refreshToken = require('./refreshToken.js')
